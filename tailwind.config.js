2381bed1e6c377b7d8c38428b25959a79dbfcff4
toyoshun <57857467+toyoshun@users.noreply.github.com>
2025-07-30 00:01:35 +1000

    Add fade animations

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'translateY(1rem)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.5s ease-out',
+      },
+    },
+  },
+  plugins: [],
+}
